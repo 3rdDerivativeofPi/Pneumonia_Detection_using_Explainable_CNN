b248a8e923cf659a486222272cf6f9b39d2df638
3rdDerivativeofPi <hoangquangdldcarchive@gmail.com>
2025-07-20 03:30:53 +0700

    Hyperparams placeholder

--- config.py
+++ config.py
@@ -1,3 +1,12 @@
 hyperparameters = {
-    
+    "num_epochs": 10,
+    "batch_size": 64,
+    "image_size": 224,
+    "fc1_size": 512,
+    "lr": 0.001,
+    "model": "resnet50",
+    "scheduler": "CosineAnnealingLR10",
+    "balance": True,
+    "early_stopping_patience": float("inf"),
+    "use_best_checkpoint": False
 }
