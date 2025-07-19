723d560c7951418ec57d0d2ad9c239dc92f24a85
3rdDerivativeofPi <hoangquangdldcarchive@gmail.com>
2025-07-19 22:11:39 +0700

    Fixed some errors

--- dataloader.py
+++ dataloader.py
@@ -8,6 +8,7 @@ from torch.utils.data import random_split
 from torch.utils.data import WeightedRandomSampler
 import pytorch_lightning as pl
 import albumentations as A
+from albumentations.pytorch import ToTensorV2
 
 class CustomImageFolder(torch.utils.data.Dataset):
     def __init__(self, root, transform=None, is_valid_file=None):
@@ -37,7 +38,7 @@ class PneumoniaDataModule(pl.LightningDataModule):
             A.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1, p=1),
             A.ShiftScaleRotate(shift_limit=0.1, scale_limit=0, rotate_limit=0, p=0.5),
             A.Perspective(scale=(0.05, 0.15), keep_size=True, p=0.5),
-            A.Resize(height=h["image_size"], width=h["image_size"]),
+            A.Resize(height=self.hyperparams["image_size"], width=self.hyperparams["image_size"]),
             A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
             ToTensorV2()
         ])  
@@ -59,58 +60,58 @@ class PneumoniaDataModule(pl.LightningDataModule):
         
         self.test_dataset = CustomImageFolder(self.data_dir+"test/", transform=data_transform_val_alb, is_valid_file=lambda x: self._is_image_file(x))
         
-        def train_dataloader(self):
-            if self.hyperparams["balance"]:
-                sampler = self._create_weighted_sampler(self.train_dataset)
-                return torch.utils.data.DataLoader(self.train_dataset, batch_size=self.hyperparams["batch_size"], sampler=sampler, num_workers=4)
-            else:
-                return torch.utils.data.DataLoader(self.train_dataset, batch_size=self.hyperparams["batch_size"], shuffle=True, num_workers=4)
-            
-        def val_dataloader(self):
-            return torch.utils.data.DataLoader(self.val_dataset, batch_size=self.hyperparams["batch_size"], num_workers=4)
+    def train_dataloader(self):
+        if self.hyperparams["balance"]:
+            sampler = self._create_weighted_sampler(self.train_dataset)
+            return torch.utils.data.DataLoader(self.train_dataset, batch_size=self.hyperparams["batch_size"], sampler=sampler, num_workers=4)
+        else:
+            return torch.utils.data.DataLoader(self.train_dataset, batch_size=self.hyperparams["batch_size"], shuffle=True, num_workers=4)
         
-        def test_dataloader(self):
-            return torch.utils.data.DataLoader(self.test_dataset, batch_size=self.hyperparams["batch_size"], num_workers=4)
+    def val_dataloader(self):
+        return torch.utils.data.DataLoader(self.val_dataset, batch_size=self.hyperparams["batch_size"], num_workers=4)
+    
+    def test_dataloader(self):
+        return torch.utils.data.DataLoader(self.test_dataset, batch_size=self.hyperparams["batch_size"], num_workers=4)
+    
+    def _extract_patient_id(self, filename):
+        patient_id = filename.split('_')[0].replace("person", "")
+        return patient_id
+    
+    def _is_image_file(self, filepath):
+        return filepath.lower().endswith((".jpeg", ".jpg", ".png"))
+    
+    def _split_file_names(self, input_folder, val_split_ratio):
+        # The PNEUMONIA files contains patient ids, so we have to split by patient to avoid data leakage
+        pneumonia_patient_ids = set([self._extract_patient_id(filename) for filename in os.listdir(os.path.join(input_folder, "PNEUMONIA"))])
+        pneumonia_val_patient_ids = random.sample(list(pneumonia_patient_ids), int(val_split_ratio * len(pneumonia_patient_ids)))
         
-        def _extract_patient_id(self, filename):
-            patient_id = filename.split('_')[0].replace("person", "")
-            return patient_id
+        pneumonia_val_filenames = []
+        pneumonia_train_filenames = []
         
-        def _is_image_file(self, filepath):
-            return filepath.lower().endswith((".jpeg", ".jpg", ".png"))
+        for filename in os.listdir(os.path.join(input_folder, "PNEUMONIUA")):
+            if self._is_image_file(filename):
+                patient_id = self._extract_patient_id(filename)
+                if patient_id in pneumonia_val_patient_ids:
+                    pneumonia_val_filenames.append(os.path.join(input_folder, "PNEUMONIA", filename))
+                else:
+                    pneumonia_train_filenames.append(os.path.join(input_folder, "PNEUMONIA", filename))
+                    
+        # The NORMAL files doesn't contain patient ids, so we can split them randomly
+        normal_filenames = [os.path.join(input_folder, "NORMAL", filename) for filename in os.listdir(os.path.join(input_folder, "NORMAL"))]
+        normal_filenames = [filename for filename in normal_filenames if self._is_image_file(filename)]
         
-        def _split_file_names(self, input_folder, val_split_ratio):
-            # The PNEUMONIA files contains patient ids, so we have to split by patient to avoid data leakage
-            pneumonia_patient_ids = set([self._extract_patient_ids(filename) for filename in os.listdir(os.path.join(input_folder, "PNEUMONIA"))])
-            pneumonia_val_patient_ids = random.sample(list(pneumonia_patient_ids), int(val_split_ratio * len(pneumonia_patient_ids)))
-            
-            pneumonia_val_filenames = []
-            pneumonia_train_filenames = []
-            
-            for filename in os.listdir(os.path.join(input_folder, "PNEUMONIUA")):
-                if self._is_image_file(filename):
-                    patient_id = self._extract_patient_id(filename)
-                    if patient_id in pneumonia_val_patient_ids:
-                        pneumonia_val_filenames.append(os.path.join(input_folder, "PNEUMONIA", filename))
-                    else:
-                        pneumonia_train_filenames.append(os.path.join(input_folder, "PNEUMONIA", filename))
-                        
-            # The NORMAL files doesn't contain patient ids, so we can split them randomly
-            normal_filenames = [os.path.join(input_folder, "NORMAL", filename) for filename in os.listdir(os.path.join(input_folder, "NORMAL"))]
-            normal_filenames = [filename for filename in normal_filenames if self._is_image_file(filename)]
-            
-            normal_val_filenames = random.sample(normal_filenames, int(val_split_ratio * len(normal_filenames)))
-            normal_train_filenames = list(set(normal_filenames) - set(normal_val_filenames))
-            
-            train_filenames = pneumonia_train_filenames + normal_train_filenames
-            val_filenames = pneumonia_val_filenames + normal_val_filenames
-            return train_filenames, val_filenames
+        normal_val_filenames = random.sample(normal_filenames, int(val_split_ratio * len(normal_filenames)))
+        normal_train_filenames = list(set(normal_filenames) - set(normal_val_filenames))
+        
+        train_filenames = pneumonia_train_filenames + normal_train_filenames
+        val_filenames = pneumonia_val_filenames + normal_val_filenames
+        return train_filenames, val_filenames
         
     def _create_weighted_sampler(self, dataset):
         targets = dataset.targets
         class_counts = np.bincount(targets)
         class_weights = 1.0 / class_counts
         weights = [class_weights[label] for label in targets]
-        sampler = WeightedRandomSampler(weights, len(weights))
+        sampler = WeightedRandomSampler(weights=weights, num_samples=len(weights))
         return sampler
             
